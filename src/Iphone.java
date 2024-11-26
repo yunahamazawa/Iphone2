e54e4d021fac4950dc5eeaeaa0b959e2cb5a995b
yunahamazawa <yunahamazawa821@gmail.com>
2024-11-26 21:52:59 +0900

    演習③

--- src/Iphone.java
+++ src/Iphone.java
@@ -1,4 +1,10 @@
 
 public class Iphone {
-
+	public static void main(String[] args){
+		SmartPhone iphone = new SmartPhone();
+		iphone.play();
+		iphone.stop();
+		iphone.next();
+		iphone.back();
+		}
 }
